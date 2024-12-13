ced669f0b69b689deff4b4ee35462198b63c0dc9
Valkirus <jirakorn.sukmee@gmail.com>
2024-12-13 15:38:55 +0700

    fixed education

--- src/containers/education/Educations.js
+++ src/containers/education/Educations.js
@@ -3,20 +3,6 @@ import "./Educations.css";
 import DegreeCard from "../../components/degreeCard/DegreeCard.js";
 import { degrees } from "../../portfolio";
 import { Fade } from "react-reveal";
-import backgroundImage from "../../assets/images/transcript.png"; // Import the image
-
-const backgroundStyle = {
-  position: "static",
-  height: "100vh", // Full viewport height
-  width: "100%", // Full viewport width
-  backgroundImage: `url(${backgroundImage})`, // Set the background image
-  backgroundSize: "cover", // Ensure the image covers the container
-  backgroundPosition: "top", // Center the image
-  backgroundRepeat: "no-repeat", // Prevent repeating
-
-  zIndex: -1, // Send it behind the content
-};
-
 
 class Educations extends Component {
   render() {
