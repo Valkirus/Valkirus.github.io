46e393ac4e14f3452e65b8a510c1c18b2721a725
Jirakorn Sukmee <jirakornsukmee@Jirakorns-MacBook-Air.local>
2025-01-18 17:05:31 +0700

    testing again

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,20 +2,18 @@ import React, { Component } from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Home from "../pages/home/HomeComponent";
 import Education from "../pages/education/EducationComponent";
-//projects
-import Joycon from "../pages/Joycon/joycon";
-import Hexapod from "../pages/Hexapod/Hexapod";
-import LearningHexapod from "../pages/LearningHexapod/learningHexapod";
-import MorphingHexapod from "../pages/MorphingHexapod/MorphingHexapod";
-
-//some new stuff
-//testing some more new stuff
 import Opensource from "../pages/opensource/Opensource";
 import Contact from "../pages/contact/ContactComponent";
 import About from "../pages/about/aboutComponent";
 import Projects from "../pages/projects/Projects";
 import { settings } from "../portfolio.js";
 import Error404 from "../pages/errors/error404/Error";
+
+//projects
+import Joycon from "../pages/Joycon/joycon";
+import Hexapod from "../pages/Hexapod/Hexapod";
+import LearningHexapod from "../pages/LearningHexapod/learningHexapod";
+import MorphingHexapod from "../pages/MorphingHexapod/MorphingHexapod";
 
 export default class Main extends Component {
   render() {
