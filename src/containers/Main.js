5ad10ebba9dbd49e130e21b3161c14f0a7c34ce1
Valkirus <jirakorn.sukmee@gmail.com>
2025-01-18 17:25:50 +0700

    removed tests

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -8,7 +8,7 @@ import Projects from "../pages/projects/Projects";
 import { settings } from "../portfolio.js";
 import Error404 from "../pages/errors/error404/Error";
 
-//projects test 32 tstes
+//projects
 import Joycon from "../pages/Joycon/joycon";
 import Hexapod from "../pages/Hexapod/Hexapod";
 import LearningHexapod from "../pages/LearningHexapod/learningHexapod";
